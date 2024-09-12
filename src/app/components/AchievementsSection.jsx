a1900d1648b9d75111e0b981f83763246a769b55
Mateo <matefernandezc@gmail.com>
2024-09-12 18:03:21 -0300

    CV

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -3,9 +3,7 @@ import React from "react";
 import dynamic from "next/dynamic";
 
 const AnimatedNumbers = dynamic(
-  () => {
-    return import("react-animated-numbers");
-  },
+  () => import("react-animated-numbers"),
   { ssr: false }
 );
 
@@ -29,26 +27,23 @@ const AchievementsSection = () => {
           return (
             <div
               key={index}
-              className="flex flex-col items-center justify-center mx-2 my-2 sm:my-0"
+              className="flex flex-col items-center justify-center mx-4 my-2 sm:my-0"
             >
-              <h2 className="text-white text-4xl font-bold flex flex-row">
-                {achievement.prefix}
+              <h2 className="text-white text-4xl font-bold flex flex-row items-baseline">
                 <AnimatedNumbers
                   includeComma
                   animateToNumber={parseInt(achievement.value)}
                   locale="en-US"
                   className="text-white text-4xl font-bold"
-                  configs={(_, index) => {
-                    return {
-                      mass: 1,
-                      friction: 100,
-                      tensions: 140 * (index + 1),
-                    };
-                  }}
+                  configs={(_, index) => ({
+                    mass: 1,
+                    friction: 100,
+                    tensions: 140 * (index + 1),
+                  })}
                 />
                 {achievement.postfix}
               </h2>
-              <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+              <p className="text-[#ADB7BE] text-base mt-1">{achievement.metric}</p>
             </div>
           );
         })}
