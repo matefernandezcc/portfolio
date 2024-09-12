f3b431ebb7cf715297b8f48007e97858599d5689
Mateo <matefernandezc@gmail.com>
2024-09-12 17:42:14 -0300

    Fix CV not downloading

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -45,7 +45,7 @@ const HeroSection = () => {
             Hire Me
           </Link>
           <a
-            href="./src/app/components/FernandezCruzMateo-CV.pdf"
+            href="./images/FernandezCruzMateo-CV.pdf"
             download="CV_Mateo_Fernandez.pdf"
             className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
           >
