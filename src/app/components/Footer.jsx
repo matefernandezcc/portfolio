d0c6fabaf02bf93aaebcd3ca84c1b365c85ca6bb
Mateo <matefernandezc@gmail.com>
2024-09-12 17:35:26 -0300

    logo

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,9 +3,13 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
-        <p className="text-slate-600">All rights reserved.</p>
+      <div className="container p-12 flex justify-between items-center">
+        <img
+          src="./images/logo.png"
+          alt="Logo"
+          className="w-8 h-8"
+        />
+        <p className="text-slate-600">copyright © 2024 all rights reserved.</p>
       </div>
     </footer>
   );
