5125e63baa1feaa6c5aa40ea05934fe698b68d79
Mateo <matefernandezc@gmail.com>
2024-09-12 18:29:43 -0300

    CV NOT DOWNLOADING

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -19,7 +19,7 @@ const HeroSection = () => {
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
             </span>
-            <br></br>
+            <br />
             <TypeAnimation
               sequence={[
                 "Mateo",
@@ -38,22 +38,24 @@ const HeroSection = () => {
             📍 From CABA, Buenos Aires, Argentina.
           </p>
           <div>
-          <Link
-            href="https://www.linkedin.com/in/mateo-fernandez-14b625265/"
-            className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
-          >
-            Hire Me
-          </Link>
-          <a
-            href="./images/CV.pdf"
-            download="CV_Mateo_Fernandez.pdf"
-            className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
-          >
-            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-              Download CV
-            </span>
-          </a>
-        </div>
+            <Link
+              href="https://www.linkedin.com/in/mateo-fernandez-14b625265/"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <a
+              href="./images/CV.pdf"
+              download="CV_Mateo_Fernandez.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </a>
+          </div>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
